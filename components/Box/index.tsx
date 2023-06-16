3f31e15e89d5732d7abd19ddbbd78a62416a1642
kyok01 <kazutakayokoils@gmail.com>
2023-06-17 06:41:37 +0900

    make box component

---
+++ components/Box/index.tsx
@@ -0,0 +1,14 @@
+export const Box = () => {
+  return (
+    <div className='flex items-center justify-between rounded-md border-2 border-solid border-neutral p-2'>
+      <div className='font-bold'>Resort X - Plan A</div>
+      <div className='flex items-center justify-around gap-4'>
+        <div>4333/5000</div>
+        <div>2023/11/13</div>
+        <div>
+          <button className='btn-primary btn'>送付</button>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ pages/company-mypage/index.tsx
@@ -0,0 +1,15 @@
+import { Box } from '../../components/Box';
+import { MenuBar } from '../../components/MenuBar';
+
+const CompanyTop: NextPage = () => {
+  return (
+    <>
+      <MenuBar />
+      <div className='m-auto mt-4  w-4/5 '>
+        <Box />
+      </div>
+    </>
+  );
+};
+
+export default CompanyTop;
