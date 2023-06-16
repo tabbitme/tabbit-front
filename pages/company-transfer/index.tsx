e77ee1a5dd450bfdbb5aea3bc15a14ea16ae2578
kyok01 <kazutakayokoils@gmail.com>
2023-06-17 06:58:34 +0900

    make company-transfer page

---
+++ pages/company-transfer/index.tsx
@@ -0,0 +1,34 @@
+import { MenuBar } from '../../components/MenuBar';
+
+const CompanyTransfer: NextPage = () => {
+  return (
+    <>
+      <MenuBar />
+      <div className='m-auto flex w-4/5 flex-col items-center justify-center gap-4'>
+        <h1 className='text-3xl'>Transfer</h1>
+        <img src='/sample-shoes.jpg' alt='shoes' className='w-3/5' />
+        <div className='w-full'>
+          <p>Quantity</p>
+          <input
+            type='text'
+            placeholder='Type here'
+            className='input-bordered input w-full '
+          />
+        </div>
+        <div className='w-full'>
+          <p>Transfer to</p>
+          <input
+            type='text'
+            placeholder='Type here'
+            className='input-bordered input w-full '
+          />
+        </div>
+        <div className='w-full'>
+          <button className='btn-primary btn w-full'>送る</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CompanyTransfer;
