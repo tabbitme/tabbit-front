a5a50b6808c742c6fde31f4cb6933d6a91519636
kyok01 <kazutakayokoils@gmail.com>
2023-06-17 06:44:42 +0900

    make boxhead

--- pages/company-mypage/index.tsx
+++ pages/company-mypage/index.tsx
@@ -1,11 +1,16 @@
 import { Box } from '../../components/Box';
+import { BoxHead } from '../../components/BoxHead';
 import { MenuBar } from '../../components/MenuBar';
 
 const CompanyTop: NextPage = () => {
   return (
     <>
       <MenuBar />
-      <div className='m-auto mt-4  w-4/5 '>
+      <div className='m-auto mt-4  flex w-4/5 flex-col gap-4'>
+        <BoxHead />
+        <Box />
+        <Box />
+        <Box />
         <Box />
       </div>
     </>
