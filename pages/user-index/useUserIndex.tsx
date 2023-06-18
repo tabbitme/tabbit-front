3ce0400f9c2f927cf1c2ac19c7e6cf701a86f438
kyok01 <kazutakayokoils@gmail.com>
2023-06-18 17:58:04 +0900

    make useUserIndex

---
+++ pages/user-index/useUserIndex.tsx
@@ -0,0 +1,9 @@
+export const useUserIndex = () => {
+  // ブロックチェーンと通信してPassとチケットの一覧情報を取得して、TicketArrとして返す
+  const ticketsArr: Ticket[] = [
+    ['/sample-shoes.jpg', 'Resort X - Plan A'],
+    ['/sample-shoes.jpg', 'Resort X - Plan A'],
+    ['/sample-shoes.jpg', 'Resort X - Plan A'],
+  ];
+  return { ticketsArr };
+};
