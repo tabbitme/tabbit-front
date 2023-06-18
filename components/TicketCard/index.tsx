6323c35f7bb2375962d8f1297ac38b6c119b9fce
kyok01 <kazutakayokoils@gmail.com>
2023-06-18 17:23:22 +0900

    remove chip on picture of ticket

--- components/TicketCard/index.tsx
+++ components/TicketCard/index.tsx
@@ -1,7 +1,6 @@
 export const TicketCard = () => {
   return (
     <div className='card relative w-72 bg-base-100 shadow-xl'>
-      <div className='badge badge-outline absolute left-3 top-3'>x200</div>
       <figure>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src='/ticket-sample.png' alt='Ticket' />
