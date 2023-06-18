ea30609a41eb346fed608353d68766d3d5997c1d
kyok01 <kazutakayokoils@gmail.com>
2023-06-18 17:31:02 +0900

    make PassBox components

--- pages/user-index/index.tsx
+++ pages/user-index/index.tsx
@@ -1,46 +1,12 @@
 import { MenuBarUser } from '../../components/MenuBarUser';
-import { TicketCard } from '../../components/TicketCard';
+import { PassBox } from '../../components/PassBox';
 
 const UserSignup: NextPage = () => {
   return (
     <>
       <MenuBarUser />
       <div>
-        <div className='m-auto flex w-4/5 justify-between rounded-md border'>
-          <div className='flex flex-col items-center pt-8'>
-            <div>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src='/pass-sample.png' alt='pass' />
-            </div>
-            <div className='text-2xl font-bold'>TABBIT #1222</div>
-          </div>
-          <div className='grid grid-cols-3 gap-4 p-4'>
-            <TicketCard
-              imgSrc='/ticket-sample.png'
-              cardTitle='Resort X - ticket A'
-            />
-            <TicketCard
-              imgSrc='/ticket-sample.png'
-              cardTitle='Resort X - ticket A'
-            />
-            <TicketCard
-              imgSrc='/ticket-sample.png'
-              cardTitle='Resort X - ticket A'
-            />
-            <TicketCard
-              imgSrc='/ticket-sample.png'
-              cardTitle='Resort X - ticket A'
-            />
-            <TicketCard
-              imgSrc='/ticket-sample.png'
-              cardTitle='Resort X - ticket A'
-            />
-            <TicketCard
-              imgSrc='/ticket-sample.png'
-              cardTitle='Resort X - ticket A'
-            />
-          </div>
-        </div>
+        <PassBox />
       </div>
     </>
   );
