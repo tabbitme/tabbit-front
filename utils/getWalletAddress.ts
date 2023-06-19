e9a8e6d021aa72350cfe128107b803c40161b630
kyok01 <kazutakayokoils@gmail.com>
2023-06-19 22:43:50 +0900

    update getWalletAddress()

--- utils/getWalletAddress.ts
+++ utils/getWalletAddress.ts
@@ -3,7 +3,7 @@ type Wallet = {
   userId: string;
 };
 // mailAddress is for example ichiro%40gmail.com
-export const getWalletAddress = async (mailAddress) => {
+export const getWalletAddress = async (mailAddress: string) => {
   const url = `https://www.patchwallet.com/_next/data/I790uNfbLI3CBBh7mFxM_/email%3A${mailAddress}.json?userId=email%3A${mailAddress}`;
   const res = await fetch(url, { method: 'GET' });
   const data: Wallet = await res.json();
