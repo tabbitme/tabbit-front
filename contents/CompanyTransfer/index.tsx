84e16c3db39f4b6ebb667efcba17be39a3460fb2
kyok01 <kazutakayokoils@gmail.com>
2023-06-19 22:53:01 +0900

    update companytransfer page

---
+++ contents/CompanyTransfer/index.tsx
@@ -0,0 +1,40 @@
+import { MenuBar } from '../../components/MenuBar';
+import { useCompanyTransfer } from './useCompanyTransfer';
+
+export const CompanyTransfer: FC<> = ({ isImageDisplayed }) => {
+  const { transferTicket } = useCompanyTransfer();
+  return (
+    <>
+      <MenuBar />
+      <div className='m-auto flex w-4/5 flex-col items-center justify-center gap-4'>
+        <h1 className='text-3xl'>Transfer</h1>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src='/sample-shoes.jpg' alt='shoes' className='w-3/5' />
+        <div className='w-full'>
+          <p>Quantity</p>
+          <input
+            type='text'
+            placeholder='Type here'
+            className='input-bordered input w-full '
+          />
+        </div>
+        <div className='w-full'>
+          <p>Transfer to</p>
+          <input
+            type='text'
+            placeholder='Type here'
+            className='input-bordered input w-full '
+          />
+        </div>
+        <div className='w-full'>
+          <button
+            className='btn-primary btn w-full'
+            onClick={() => transferTicket()}
+          >
+            送る
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
