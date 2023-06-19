b32cb658dc178d41525b37aa219df418e871eb0a
kyok01 <kazutakayokoils@gmail.com>
2023-06-19 23:26:42 +0900

    update user-index

--- pages/user-index/index.tsx
+++ pages/user-index/index.tsx
@@ -1,12 +1,8 @@
 import { NextPage } from 'next';
 
 import { UserIndex } from '../../contents/UserIndex';
-import { useUserIndex } from './useUserIndex';
 
 const UserIndexPage: NextPage = () => {
-  const { ticketsArr } = useUserIndex();
-  console.log(ticketsArr);
-
   return <UserIndex />;
 };
 
