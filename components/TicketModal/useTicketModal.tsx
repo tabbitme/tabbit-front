859257f522e6f771f95287e65d4697828213dd89
yuk6ra <yuki.imamura.all@gmail.com>
2023-06-20 22:18:04 +0900

    Update demo

--- components/TicketModal/useTicketModal.tsx
+++ components/TicketModal/useTicketModal.tsx
@@ -2,7 +2,7 @@ import { ethers, Typed } from 'ethers';
 
 interface Window {
   my_modal_3: any;
-  ethereum?: any;
+  ethereum: any;
 }
 declare var window: Window;
 
@@ -16,7 +16,43 @@ export const useTicketModal = () => {
   };
 
   const makeTicket = async () => {
-    // チケットの情報を取得
+    let signer = null;
+
+    let provider;
+    if (window.ethereum == null) {
+      // If MetaMask is not installed, we use the default provider,
+      // which is backed by a variety of third-party services (such
+      // as INFURA). They do not have private keys installed so are
+      // only have read-only access
+      console.log('MetaMask not installed; using read-only defaults');
+      //   provider = ethers.getDefaultProvider();
+    } else {
+      // Connect to the MetaMask EIP-1193 object. This is a standard
+      // protocol that allows Ethers access to make all read-only
+      // requests through MetaMask.
+      provider = new ethers.BrowserProvider(window.ethereum);
+
+      // It also provides an opportunity to request access to write
+      // operations, which will be performed by the private key
+      // that MetaMask manages for the user.
+      signer = await provider.getSigner();
+
+      const maxSupply = 100;
+      const name = '旅館A アクティビティ';
+      const description =
+        '旅館Aでは、人力の船によるアクティビティが楽しめます。美しい景色を眺めながら、穏やかな水面を漕ぐ贅沢な時間を過ごせます。ゆったりとした船旅で心身を癒し、自然と一体化する感覚を味わえます。親切な船頭が案内してくれるので安心です。この独特の体験は、日常の喧騒を忘れさせ、心をリフレッシュさせます。人力の船でのアクティビティは、静寂と調和を求める方にぴったりです。';
+      const imageUri =
+        'http://arweave.net/6fceU9xnLdnBRmMegXa9O8MTwqSVVuqYX_5nyiVLo6Q';
+      const contractAddress = '0x1464D892F270D5746C3c12C6ED48f8DCdF3e673d';
+
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      const transaction = await contract.createTicket(
+        Typed.uint256(maxSupply),
+        Typed.string(name),
+        Typed.string(description),
+        Typed.string(imageUri)
+      );
+    }
   };
 
   const clickHandler = async () => {
--- contents/CompanyTransfer/useCompanyTransfer.tsx
+++ contents/CompanyTransfer/useCompanyTransfer.tsx
@@ -1,7 +1,59 @@
+import { ethers, Typed } from 'ethers';
+
+interface Window {
+  my_modal_3: any;
+  ethereum: any;
+}
+
+declare var window: Window;
+
 export const useCompanyTransfer = () => {
   // チケットを送付する処理を書く
-  const transferTicket = () => {
-    alert('aaa');
+
+  const transferTicket = async () => {
+    const abi = [
+      'function issueTickets(uint256 ticketId, address _to, uint256 _quantity)',
+    ];
+
+    const issueTicket = async () => {
+      let signer = null;
+
+      let provider;
+      if (window.ethereum == null) {
+        // If MetaMask is not installed, we use the default provider,
+        // which is backed by a variety of third-party services (such
+        // as INFURA). They do not have private keys installed so are
+        // only have read-only access
+        console.log('MetaMask not installed; using read-only defaults');
+        // provider = ethers.getDefaultProvider();
+      } else {
+        // Connect to the MetaMask EIP-1193 object. This is a standard
+        // protocol that allows Ethers access to make all read-only
+        // requests through MetaMask.
+        provider = new ethers.BrowserProvider(window.ethereum);
+
+        // It also provides an opportunity to request access to write
+        // operations, which will be performed by the private key
+        // that MetaMask manages for the user.
+        signer = await provider.getSigner();
+
+        const ticketId = 0;
+        const to = '0x552743d6E77F37c3AE75e1238519065B14Afea61';
+        const quantity = 2;
+
+        const contractAddress = '0x1464D892F270D5746C3c12C6ED48f8DCdF3e673d';
+
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        const transaction = await contract.issueTickets(
+          Typed.uint256(ticketId),
+          Typed.address(to),
+          Typed.uint256(quantity)
+        );
+      }
+    };
+
+    await issueTicket();
   };
+
   return { transferTicket };
 };
