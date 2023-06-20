c06c30911cc4135b9842b4d7ad287168a75b7c9f
yuk6ra <yuki.imamura.all@gmail.com>
2023-06-20 21:18:24 +0900

    Add sample tickets

--- contents/CompanyTop/useCompanyTop.tsx
+++ contents/CompanyTop/useCompanyTop.tsx
@@ -3,9 +3,15 @@ type Ticket = [string, string];
 export const useCompanyTop = () => {
   // ブロックチェーンと通信してチケットの一覧情報を取得して、TicketArrとして返す
   const ticketsArr: Ticket[] = [
-    ['/sample-shoes.jpg', 'Resort X - Plan A'],
-    ['/sample-shoes.jpg', 'Resort X - Plan A'],
-    ['/sample-shoes.jpg', 'Resort X - Plan A'],
+    [
+      'http://arweave.net/mGUEh929a3A_qTondUKvWJFjoR3942irW3eshd9l8Jk',
+      '旅館A 2day',
+    ],
+    [
+      'http://arweave.net/6fceU9xnLdnBRmMegXa9O8MTwqSVVuqYX_5nyiVLo6Q',
+      '旅館A アクティビティ',
+    ],
   ];
+
   return { ticketsArr };
 };
