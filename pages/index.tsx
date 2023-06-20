a8ec6d8235313e7718b41c8800a44f9a932827d5
kyok01 <kazutakayokoils@gmail.com>
2023-06-20 17:18:19 +0900

    clean pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,9 @@
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
 
 import { CompanyTop } from '../contents/CompanyTop';
 
 const Home: NextPage = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/proxy', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            userIds: 'email:ur@mom69.com',
-          }),
-        });
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return <CompanyTop />;
-  // return (
-  //   <div>
-  //     <h1>Response from API</h1>
-  //     <pre>{JSON.stringify(data, null, 2)}</pre>
-  //   </div>
-  // );
 };
 
 export default Home;
