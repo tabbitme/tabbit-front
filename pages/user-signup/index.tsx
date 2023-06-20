d4394b5911a1de8a8a8c3190e53b90b0d4fb4118
kyok01 <kazutakayokoils@gmail.com>
2023-06-20 16:44:26 +0900

    update user-signup logic

--- pages/user-signup/index.tsx
+++ pages/user-signup/index.tsx
@@ -1,9 +1,51 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 import { MenuBarUser } from '../../components/MenuBarUser';
 
+type ResData = {
+  users: Users[];
+};
+
+type Users = {
+  userId: string;
+  accountAddress: string;
+};
+
 const UserSignup: NextPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    setInputValue(() => e.target.value);
+  };
+
+  const fetchAddressBasedOnMail = async () => {
+    try {
+      const response = await fetch('/api/proxy', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userIds: `email:${inputValue}`,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data: ResData = await response.json();
+      console.log(data);
+      const address = data.users[0].accountAddress;
+      console.log(address);
+      router.push(`/user-index?address=${address}`);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
+
   return (
     <>
       <MenuBarUser isSignUpButtonDisplayed={true} />
@@ -23,9 +65,14 @@ const UserSignup: NextPage = () => {
                 type='text'
                 placeholder='メールアドレス'
                 className='input-bordered input w-3/5'
+                value={inputValue}
+                onChange={handleChange}
               />
-              <button className='btn-primary btn w-3/5'>
-                アカウントを作成する
+              <button
+                className='btn-primary btn w-3/5'
+                onClick={() => fetchAddressBasedOnMail()}
+              >
+                アカウントを作成してログイン
               </button>
               <Link href='/' className='underline'>
                 既にアカウントを作成している方はこちら
