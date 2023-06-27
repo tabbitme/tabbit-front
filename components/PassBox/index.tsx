0df438ce4b7d480d540678645c83aad61544204e
kyok01 <kazutakayokoils@gmail.com>
2023-06-27 11:17:12 +0900

    update image

--- components/PassBox/index.tsx
+++ components/PassBox/index.tsx
@@ -2,15 +2,15 @@ import { TicketCard } from '../TicketCard';
 
 export const PassBox = () => {
   return (
-    <div className='m-auto mt-8 flex w-10/12 justify-between gap-4 rounded-md border bg-neutral-content'>
-      <div className='flex basis-1/4 flex-col items-center pt-8'>
+    <div className='m-auto mt-8 flex w-11/12 justify-between gap-16 rounded-md border bg-neutral-content'>
+      <div className='flex basis-1/4 flex-col items-center pl-2 pt-8'>
         <div>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src='/pass-sample.png' alt='pass' />
+          <img src='/hokkaido-hotei.png' alt='pass' className='w-96' />
         </div>
         <div className='text-2xl font-bold'>ニセコホテル #1222</div>
       </div>
-      <div className='grid basis-3/4 grid-cols-3 gap-2 pr-4 pt-8'>
+      <div className='grid basis-3/4 grid-cols-3 gap-y-4 pr-2 pt-10'>
         <TicketCard imgSrc='/ticket_10.png' cardTitle='ペア宿泊券一泊二日' />
         <TicketCard
           imgSrc='/ticket_11.png'
